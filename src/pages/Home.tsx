f1503f70f3d0fc948c73d4079eeb6395d82a8384
Chang Yeol <ckdduf138@naver.com>
2025-03-12 18:05:56 +0900

    feat: 내용 복사 밑 토스트알림 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,8 @@ import React, { useEffect, useState } from 'react';
 import ReportForm from '../components/ReportForm';
 import ReportViewer from '../components/ReportViewer';
 
-import { deleteReport, getReports, saveReport } from '../utils/storage';
+import { deleteReport, getReports, saveReport, copyReport } from '../utils/storage';
+import { ToastContainer } from 'react-toastify';
 
 const Home: React.FC = () => {
   const [reports, setReports] = useState<{ startTime: string; endTime: string; content: string; }[]>([]);
@@ -24,13 +25,17 @@ const Home: React.FC = () => {
     setReports(getReports());
   };
 
+  const handleExport = () => {
+    copyReport();
+  };
+
   return (
     <div className="min-h-screen flex items-start justify-center relative">
       <div className="p-6 w-full max-w-7xl">
         <h1 className="text-2xl font-bold mb-4 text-center text-black">Daily Report</h1>
 
         <ReportViewer reports={reports} delete_report={handleDelete}/>
-        
+
         {isModalOpen && (
           <ReportForm
             onSubmit={handleAddReport}
@@ -38,13 +43,23 @@ const Home: React.FC = () => {
           />
         )}
 
+        {/* 업로드 버튼 */}
+        <button
+          className="fixed bottom-8 left-8 w-16 h-16 bg-[#DDDDDD] flex items-center justify-center rounded-full shadow-lg"
+          onClick={() => handleExport()}
+        >
+          <img src='/images/home/ic-export.svg'/>
+        </button>
+
         {/* 플로팅 버튼 */}
         <button
-          className="fixed bottom-8 right-8 w-16 h-16 bg-black text-white text-4xl flex items-center justify-center rounded-full shadow-lg"
+          className="fixed bottom-8 right-8 w-16 h-16 bg-black flex items-center justify-center rounded-full shadow-lg"
           onClick={() => setIsModalOpen(true)}
         >
           <img src='/images/home/ic-plus.svg'/>
         </button>
+
+        <ToastContainer />
       </div>
     </div>
   );
--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,4 +1,8 @@
-const STORAGE_KEY = 'daily_reports';
+import { toast } from "react-toastify";
+
+import { formatTime } from "./transalte";
+
+export const STORAGE_KEY = 'daily_reports';
 
 // 리포트를 저장하는 함수
 export const saveReport = (startTime: string, endTime: string, content: string) => {
@@ -32,3 +36,31 @@ export const deleteReport = (index: number) => {
   reports.splice(index, 1);
   localStorage.setItem(STORAGE_KEY, JSON.stringify(reports));
 };
+
+// 리포트를 복사하는 함수
+export const copyReport = () => {
+  const reports = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+
+  const reportText = reports.map((report: { startTime: string, endTime: string, content: string }) => {
+    const formattedStartTime = formatTime(report.startTime);
+    const formattedEndTime = formatTime(report.endTime);
+    return `${formattedStartTime} ~ ${formattedEndTime}\n${report.content}\n\n`;
+  }).join('');
+
+  navigator.clipboard.writeText(reportText)
+    .then(() => {
+      toast.success('클립보드에 복사되었습니다', {
+        position: 'top-center',
+        autoClose: 3000,
+        hideProgressBar: true,
+      });
+    })
+    .catch((error) => {
+      console.error('클립보드 복사 실패:', error);
+      toast.error('복사하는데 실패했습니다', {
+        position: 'top-center',
+        autoClose: 3000,
+        hideProgressBar: true,
+      });
+    });
+}
