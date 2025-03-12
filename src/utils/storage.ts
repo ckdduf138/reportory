701b41baa3caa32c345603aa670bc7cd2af3111c
Chang Yeol <ckdduf138@naver.com>
2025-03-12 17:08:24 +0900

    feat: 시간 순으로 저장되게 수정

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -3,8 +3,20 @@ const STORAGE_KEY = 'daily_reports';
 // 리포트를 저장하는 함수
 export const saveReport = (startTime: string, endTime: string, content: string) => {
   const reports = getReports();
-  const newReport = {startTime, endTime, content };
+  const newReport = { startTime, endTime, content };
+
   reports.push(newReport);
+
+  reports.sort((a, b) => {
+    const [aHours, aMinutes] = a.startTime.split(':').map(Number);
+    const [bHours, bMinutes] = b.startTime.split(':').map(Number);
+
+    const aTime = new Date(0, 0, 0, aHours, aMinutes); // 시간을 Date 객체로 변환
+    const bTime = new Date(0, 0, 0, bHours, bMinutes);
+
+    return aTime.getTime() - bTime.getTime(); // 시간순으로 정렬
+  });
+
   localStorage.setItem(STORAGE_KEY, JSON.stringify(reports));
 };
 
